f675a75488d8b35e6c4d4fd6d308c221c9b06c80
Rishabh <jharishabh2k3@gmail.com>
2022-08-09 18:38:55 +0530

    update

--- 27.py
+++ 27.py
@@ -1,10 +1,23 @@
-numlist = list()
-while True:
-    inp = input('enter a number: ')
-    if inp == 'done':
-        break
-    value = float(inp)
-    numlist.append(value)
-average = sum(numlist) / len(numlist)
-print(numlist)
-print(average)
+# numlist = list()
+# while True:
+#     inp = input('enter a number: ')
+#     if inp == 'done':
+#         break
+#     value = float(inp)
+#     numlist.append(value)
+# average = sum(numlist) / len(numlist)
+# print(numlist)
+# print(average)
+# print(set(numlist))
+# if len(numlist) > len(set(numlist)):
+#     print(numlist)
+#     pass
+#
+def unique_list(l):
+  x = []
+  for a in l:
+    if a not in x:
+      x.append(a)
+  return x
+
+print(unique_list([1,2,3,3,3,3,4,5])) 
---
+++ 28.py
@@ -0,0 +1,4 @@
+import math
+a = input("enter a number: ")
+b = math.lcm(9, 12)
+print(b)
