bdb5644f2d3357b1dc383487445ae82d99be0be2
Rishabh <jharishabh2k3@gmail.com>
2022-08-09 19:44:09 +0530

    update

--- 28.py
+++ 28.py
@@ -1,4 +1,21 @@
-import math
-a = input("enter a number: ")
-b = math.lcm(9, 12)
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ]
+b = []
+for num in a:
+    if num % 2 == 0:
+        b.append(num)
+        b.sort()
 print(b)
+
+
+# b = []
+# c = []
+# while True:
+#     a = input('enter the numbers: ')
+#     if a == 'done':
+#         break
+#     b.append(a)
+#     for num in b:
+#         if num % 2 == 0:
+#             c.append(num)
+# print(c)
+
