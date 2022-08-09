43adabf5bb42b65477f7bc67fd2cf4f88b8e0c3b
Rishabh <jharishabh2k3@gmail.com>
2022-08-09 10:21:17 +0530

    update

--- 24.py
+++ 24.py
@@ -1,9 +1,9 @@
 count = 0
 total = 0
 
-a = input("Enter a file name: ")
+a = input('Enter a file name: ')
 if a == 'na na boo boo':
-    print('NA NA BOO BOO TO YOU - You have been pun\'d!')
+    print("NA NA BOO BOO TO YOU - You have been pun'd!")
     exit()
 b = open(a)
 for line in b:
