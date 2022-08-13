b2bc17cecfac0b8b7d1d62d26364d09211489e4b
Rishabh <jharishabh2k3@gmail.com>
2022-08-13 12:12:12 +0530

    update

--- 35.py
+++ 35.py
@@ -1,3 +1,16 @@
 import string
-a = string.punctuation
-print(a)
+
+a = open('romeo')
+b = dict()
+for line in a:
+    line = line.rstrip()
+    line = line.translate(line.maketrans('', '', string.punctuation))
+    words = line.split()
+    for word in words:
+        if word not in b:
+            b[word] = 1
+        else:
+            b[word] += 1
+
+print(b)
+string.punctuation
