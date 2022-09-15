41ae464327e7492c3550dede60e54bee5d8efff5
Rishabh <jharishabh2k3@gmail.com>
2022-09-15 17:57:14 +0530

    update

--- 39.py
+++ 39.py
@@ -1,2 +1,34 @@
-import this
-print(this)
+# import this
+# print(this)
+# a = []
+# b = ["Holy Grail"]
+# print(type(b))
+# c = [1, 3, 5] 
+# print(type(c))
+# d = 2 + 4 + 24
+# e = 21 // 2 / 5
+# f = print(type(e))
+# g = 2 + 3 * 3 - 7
+# h = print(type(g))
+# i = ()
+# print(type(i))
+# j = []
+# k = j.append('a')
+# print(j)
+# l = [10, 20, 30]
+# m = l.pop(2)
+# print(m)
+# n = help(list)
+
+# help(dir)
+# a = dir(dict)
+# print(a)
+
+# def sum(a: float, b: float, c: float) -> None:
+#     su = a + b + c
+#     print(su)
+#     return 
+# sum(101, 102, 103.5)
+# sum(True, False, True)
+
+def
